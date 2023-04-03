08fc9871e107884ef8b54040c440c04f68981572
huangzhou <huangzhou@cyek.com>
2023-04-03 10:20:37 +0800

    debug

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '1bc1b620493b6f853e9443146ef6b4d3535a9d0d',
+        'name' => 'yileus/laravel-douyin-master',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'yileus/laravel-douyin-master' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '1bc1b620493b6f853e9443146ef6b4d3535a9d0d',
+            'dev_requirement' => false,
+        ),
+    ),
+);
